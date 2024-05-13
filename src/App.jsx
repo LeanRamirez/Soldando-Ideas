88780eeb2b36794d935746ed04ad804b9d2ee348
LeanRamirez <maxi3230lmr@gmail.com>
2024-05-13 11:25:54 -0300

    barra de navegacion con funcionalidad terminada

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 // import style from "./App.css"
 
 
--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,22 +1,37 @@
 import React from "react";
+
 import style from "./navBar.module.css";
 
 import logo from "../../assets/logo.png";
 
 const NavBar =()=>{
+
   return(
     <nav className="navbar navbar-expand-md navbar-dark">
       <div className={`container-fluid ${style.container}`}>
           <a className="navbar-brand">
             <img className={style.logo} src={logo} alt="logo"/>
           </a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+        <button className="navbar-toggler" 
+        type="button" 
+        data-bs-toggle="collapse" 
+        data-bs-target="#navbarToggler" 
+        aria-controls="navbarToggler" 
+        aria-expanded="false" 
+        aria-label="Toggle navigation" 
+        >
+          <span className="navbar-toggler-icon">
+            
+          </span>
         </button>
+        
         <div className={`collapse navbar-collapse ${style.menu}`} id="navbarToggler">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          
+          <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link" aria-current="page" href="#inicio">INICIO</a>
+              <a className="nav-link" 
+              aria-current="page" 
+              href="#inicio">INICIO</a>
             </li> 
             <li className="nav-item">
               <a className="nav-link" href="#trabajos">TRABAJO</a>
@@ -26,6 +41,7 @@ const NavBar =()=>{
             </li>
           </ul>
         </div>
+      
       </div>
 </nav>
   )
