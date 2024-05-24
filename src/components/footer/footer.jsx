dbc5ebb074cbfaf24d6b71cb1ab101e6948a89d2
Soledad Ramirez <liasoledad27@gmail.com>
2024-05-23 22:15:08 -0300

    style footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMapMarkerAlt, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
-
+import logoTransp from "../../assets/logoTransp.png";
 import style from "./footer.module.css";
+import logoSem from "../../assets/logoSem1.png"
 
 const Footer = () => {
 
@@ -11,6 +12,10 @@ const Footer = () => {
     return (
         <footer className={style.footer}>
             <div className={style.container}>
+                <div className={style.logoContainer}>
+                <img className={style.logo} src={logoTransp} alt="logo"/>
+                <h3 className={style.brand}>SOLDANDO IDEAS</h3>
+                </div>
                 <div className={style.column}>
                     <h2>Contacto</h2>
                     <p><FontAwesomeIcon className={style.icon} icon={faMapMarkerAlt}/> Dirección: Cuba 1071, El talar, Tigre, Buenos Aires.</p>
@@ -20,24 +25,26 @@ const Footer = () => {
                     <FontAwesomeIcon className={style.icon} icon={faMapMarkerAlt}/>Ver ubicación</a>
                 </div>
                 <div className={style.instagram}>
-                    <h2>Síguenos</h2>
-                    <ul>
+                    <h2 className={style.follow}>Seguinos</h2>
+                    <ul className={style.linkUl}>
                         <li><a href="https://www.instagram.com/soldandoideas_herreria/" target="_blank" rel="noopener noreferrer">
                             <FontAwesomeIcon className={style.icon} icon={faInstagram}/>Instagram</a></li>
                     </ul>
                 </div>
                 <div className={style.column}>
                     <h2>Enlaces</h2>
-                    <ul>
-                        <li><a href="#inicio">INICIO</a></li>
-                        <li><a href="#trabajos">QUE HACEMOS</a></li>
-                        <li><a  href="#contacto">CONTACTO</a></li>
+                    <ul className={style.linkUl}>
+                        <li><a className={style.links} href="#inicio">INICIO</a></li>
+                        <li><a className={style.links} href="#trabajos">QUE HACEMOS</a></li>
+                        <li><a  className={style.links} href="#contacto">CONTACTO</a></li>
                     </ul>
                 </div>
                 
             </div>
             <div className={style.bottom}>
-                <p>&copy; {new Date().getFullYear()} Soldando ideas. Todos los derechos reservados.
+                <p>&copy; {new Date().getFullYear()} 
+                    <img className={style.logoSem} src={logoSem} alt="" />
+                    Desarrollo web
                 </p>
             </div>
         </footer>
